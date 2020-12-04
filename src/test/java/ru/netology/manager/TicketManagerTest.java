886e3bccb47eff4d77791035680bd1927b56fc88
BethPech <colnze17-94@mail.ru>
2020-12-04 21:51:04 +0300

    Tests are corrected

--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.TicketOffer;
 import ru.netology.repository.TicketRepository;
+
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -16,7 +17,7 @@ public class TicketManagerTest {
     private TicketOffer item3 = new TicketOffer(3, 31600, "VKO", "LAX", 2565);
     private TicketOffer item4 = new TicketOffer(4, 32249, "DME", "LAX", 2495);
     private TicketOffer item5 = new TicketOffer(5, 32417, "DME", "LAX", 1455);
-    private TicketOffer item6 = new TicketOffer(6, 33771, "DME", "LAX", 2205);
+    private TicketOffer item6 = new TicketOffer(6, 33771, "DME", "ZI", 2205);
 
     @BeforeEach
     public void setUp() {
@@ -29,9 +30,44 @@ public class TicketManagerTest {
     }
 
     @Test
-    void shouldSortByPrice() {
+    void shouldFindAndSortByPrice() {
         TicketOffer[] expected = new TicketOffer[]{item1, item3, item2};
         TicketOffer[] actual = manager.findAll("VKO", "LAX");
+        Arrays.sort(actual);
+        assertArrayEquals(expected, actual);
+        System.out.print(Arrays.toString(actual));
+    }
+
+    @Test
+    void shouldNotFindAnything() {
+        TicketOffer[] expected = new TicketOffer[0];
+        TicketOffer[] actual = manager.findAll("ZI", "LAX");
+        Arrays.sort(actual);
+        assertArrayEquals(expected, actual);
+        System.out.print(Arrays.toString(actual));
+    }
+
+    @Test
+    void shouldNotFindAny() {
+        TicketOffer[] expected = new TicketOffer[0];
+        TicketOffer[] actual = manager.findAll("VKO", "ZI");
+        Arrays.sort(actual);
+        assertArrayEquals(expected, actual);
+        System.out.print(Arrays.toString(actual));
+    }
+
+    @Test
+    void shouldFindButNotSort() {
+        TicketOffer[] expected = new TicketOffer[]{item1, item2, item3};
+        TicketOffer[] actual = manager.findAll("VKO", "LAX");
+        assertArrayEquals(expected, actual);
+        System.out.print(Arrays.toString(actual));
+    }
+
+    @Test
+    void shouldFindOne() {
+        TicketOffer[] expected = new TicketOffer[]{item6};
+        TicketOffer[] actual = manager.findAll("DME", "ZI");
         Arrays.sort(actual);
         assertArrayEquals(expected, actual);
         System.out.print(Arrays.toString(actual));
